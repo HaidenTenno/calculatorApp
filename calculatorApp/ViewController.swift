dad20346b488da7dbe6d33afad9da1ed3d094d0b
Petr Tartynskikh <haiden_tenno@mail.ru>
2019-09-29 18:18:49 +0300

    Initial Commit

---
+++ calculatorApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculatorApp
+//
+//  Created by Петр Тартынских  on 29/09/2019.
+//  Copyright © 2019 Петр Тартынских . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
